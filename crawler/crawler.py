1c5abbcaf102229ff3711821608c5d7ec01e7fbd
GavinChiang <cchsuan@umich.edu>
2017-03-21 04:05:32 -0400

    parse all apple news"

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -9,6 +9,7 @@ import re
 import string
 from bs4 import BeautifulSoup
 from newspaper import Article
+import _pickle as cPickle
 
 '''
 newspaper example:
@@ -43,32 +44,39 @@ tokens_perday = {}
 tokens_perday
 key is time, value daily tokens
 '''
-while True:
-	res = requests.get("https://www.google.com/finance/company_news?q=NASDAQ%3AAAPL&ei=Ui3PWLC6EYrNjAHb-I-YBA&startdate=2017-02-01&enddate=2017-03-01&start="+str(page_index)+"&num=10")
-	soup = BeautifulSoup(res.text,"html.parser")
-		
-	news_tags = soup.find_all('div',attrs={'class': 'g-section news sfe-break-bottom-16'})
-	if len(news_tags) == 0:
-		print('No More News at page ' + str(page_index))
-		break
-	page_index += 10
+urls = ['https://www.google.com/finance/company_news?q=NASDAQ%3AAAPL&ei=9NvQWPG2J4i3jAH12prADw&startdate=2017-01-01&enddate=2017-02-01&start=',
+		'https://www.google.com/finance/company_news?q=NASDAQ%3AAAPL&ei=Ui3PWLC6EYrNjAHb-I-YBA&startdate=2017-02-01&enddate=2017-03-01&start=']
 
-	for tag in news_tags:
-		time_tmp = str(tag.find('span',attrs={'class': 'date'}).get_text())
-		time = datetime.datetime.strptime(time_tmp,"%b %d, %Y").strftime('%Y-%m-%d')
+for url in urls:
+	page_index = 0
+	while True:
+		# res = requests.get("https://www.google.com/finance/company_news?q=NASDAQ%3AAAPL&ei=Ui3PWLC6EYrNjAHb-I-YBA&startdate=2017-02-01&enddate=2017-03-01&start="+str(page_index)+"&num=10")
+		res = requests.get(url + str(page_index)+"&num=10")
+		soup = BeautifulSoup(res.text,"html.parser")
+		news_tags = soup.find_all('div',attrs={'class': 'g-section news sfe-break-bottom-16'})
+		if len(news_tags) == 0:
+			print('No More News at page ' + str(page_index))
+			break
+		page_index += 10
 
-		t = tag.find('a',attrs={'id': 'n-cn-'})
-		title = ''.join([tag if ord(tag) < 128 else ' ' for tag in t.text])
-		
-		article = Article(t['href'])
-		article.download()
-		if article.is_downloaded:
-			article.parse()
-		
-		if time not in news_per_day:
-			news_per_day[time] = ""
-		text = ''.join([word if ord(word) < 128 else ' ' for word in article.text])
-		news_per_day[time] += str(title + " " + text + " ")
+		for tag in news_tags:
+			time_tmp = str(tag.find('span',attrs={'class': 'date'}).get_text())
+			time = datetime.datetime.strptime(time_tmp,"%b %d, %Y").strftime('%Y-%m-%d')
+
+			t = tag.find('a',attrs={'id': 'n-cn-'})
+			title = ''.join([tag if ord(tag) < 128 else ' ' for tag in t.text])
+			
+			article = Article(t['href'])
+			article.download()
+			if article.is_downloaded:
+				article.parse()
+			
+			if time not in news_per_day:
+				news_per_day[time] = ""
+			text = ''.join([word if ord(word) < 128 else ' ' for word in article.text])
+			news_per_day[time] += str(title + " " + text + " ")
+
+cPickle.dump(news_per_day, open("news_per_day.p", "wb" ) )
 
 def after_tokenize_remove_digit_and_character(token_list):
 	after_process_tokens = []
@@ -114,7 +122,8 @@ def count_daily_tokens(tokens_perday):
 		res[time] = daily_count
 	return res
 
-reader = csv.reader(open('apple_feb_daily_price.csv', 'r'))
+# reader = csv.reader(open('apple_feb_daily_price.csv', 'r'))
+reader = csv.reader(open('test_shift.csv', 'r'))
 next(reader)
 daily_price	= {}
 for row in reader:
--- crawler/get_price.py
+++ crawler/get_price.py
@@ -1,4 +1,6 @@
 import csv
+import datetime
+from pprint import pprint
 
 # 1 is increase and -1 is decrease
 def increase_or_decrease(num1, num2):
@@ -6,8 +8,9 @@ def increase_or_decrease(num1, num2):
 		return 1
 	else:
 		return -1
-
+'''
 daily_price	= {}
+
 with open('apple_feb.csv') as f:
 	reader = csv.reader(f)
 	next(f)
@@ -19,3 +22,47 @@ writer = csv.writer(open('apple_feb_daily_price.csv', 'w'))
 writer.writerow(['date', 'price'])
 for key, value in daily_price.items():
    writer.writerow([key, value])
+'''
+
+start_date = '2017-01-04'
+end_date = '2017-02-28'
+dates = []
+count = 1
+while True:
+	count += 1
+	today = start_date
+	dates.append(today)
+	start_date = (datetime.datetime.strptime(today,'%Y-%m-%d') + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+	
+	if start_date == end_date:
+		break
+
+
+daily_price	= {}
+
+with open('apple_jan_feb.csv') as f:
+	reader = csv.reader(f)
+	next(f)
+	for row in reader:
+		# print(row[0],row[1],row[4])
+		daily_price[row[0]] = increase_or_decrease(row[1], row[4])
+
+for date in dates:
+	if date not in daily_price:
+		previous_date = (datetime.datetime.strptime(date,'%Y-%m-%d') + datetime.timedelta(days=-1)).strftime('%Y-%m-%d')
+		daily_price[date] = daily_price[previous_date]
+
+writer = csv.writer(open('test_not_shift.csv', 'w'))
+writer.writerow(['date', 'price'])
+for date in sorted(daily_price.keys()):
+   writer.writerow([date, daily_price[date]])
+
+shift_next_previous_date_price = {}
+for date, price in daily_price.items():
+	shift_date = (datetime.datetime.strptime(date,'%Y-%m-%d') + datetime.timedelta(days=-1)).strftime('%Y-%m-%d')
+	shift_next_previous_date_price[shift_date] = price
+
+writer = csv.writer(open('test_shift.csv', 'w'))
+writer.writerow(['date', 'price'])
+for date in sorted(shift_next_previous_date_price.keys()):
+   writer.writerow([date, shift_next_previous_date_price[date]])
