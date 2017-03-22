5725e53ad6661a468c0fb560efa19c5750e25083
Ting-Wei Cho <louischo@flux-login2.arc-ts.umich.edu>
2017-03-22 16:17:14 -0400

    Nothing important

--- util/gensim_dic2csv.py
+++ util/gensim_dic2csv.py
@@ -11,7 +11,7 @@ else:
 
 mapping = dictionary.token2id
 
-word_dic_writer = csv.writer(open('gensim_word_dic.csv', 'w'))
+word_dic_writer = csv.writer(open('../data/gensim_data/gensim_word_dic.csv', 'w'))
 
 for word, idx in mapping.items():
 	word_dic_writer.writerow([word, idx])
