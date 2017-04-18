0c6b891da998b8bc9bd518b6981adab3473583d9
louischo <louischo@umich.edu>
2017-04-17 23:10:17 -0400

    combine data and label. modify model

--- models.py
+++ models.py
@@ -20,47 +20,80 @@ from sklearn.feature_extraction.text import CountVectorizer
 #==============================================================================
 # Define parameters
 #==============================================================================
-apple_data_path = 'data/apple_all.txt'
-alphabet_data_path = 'data/alphabet_all.txt'
-yahoo_data_path = 'data/yahoo_all.txt'
-apple_labels_path = 'data/apple_labels.csv'
-alphabet_labels_path = 'data/alphabet_labels.csv'
-yahoo_labels_path = 'data/yahoo_labels.csv'
+apple_data_path_train = 'data/apple_train.txt'
+apple_data_path_test = 'data/apple_test.txt'
+alphabet_data_path_train = 'data/alphabet_train.txt'
+alphabet_data_path_test = 'data/alphabet_test.txt'
+yahoo_data_path_train = 'data/yahoo_train.txt'
+yahoo_data_path_test = 'data/yahoo_test.txt'
+
+apple_labels_path_train = 'data/apple_labels_train.csv'
+apple_labels_path_test = 'data/apple_labels_test.csv'
+alphabet_labels_path_train = 'data/alphabet_labels_train.csv'
+alphabet_labels_path_test = 'data/alphabet_labels_test.csv'
+yahoo_labels_path_train = 'data/yahoo_labels_train.csv'
+yahoo_labels_path_test = 'data/yahoo_labels_test.csv'
 stop_list_path = "data/stoplist.txt"
 
 #==============================================================================
 # Load data of different companies
 #==============================================================================
-apple_date_news_dict = read_news(apple_data_path)
-apple_labels = pd.read_csv(apple_labels_path)
-apple_date_labels_dict = dict(zip(apple_labels['date'], apple_labels['price']))
-apple_news_labels_dict = match_news_labels(apple_date_news_dict, apple_date_labels_dict)
+# Load test data
+apple_date_news_dict_train = read_news(apple_data_path_train)
+apple_labels_train = pd.read_csv(apple_labels_path_train)
+apple_date_labels_dict_train = dict(zip(apple_labels_train['date'], apple_labels_train['price']))
+apple_news_labels_dict_train = match_news_labels(apple_date_news_dict_train, apple_date_labels_dict_train)
 
-alphabet_date_news_dict = read_news(alphabet_data_path)
-alphabet_labels = pd.read_csv(alphabet_labels_path)
-alphabet_date_labels_dict = dict(zip(alphabet_labels['date'], alphabet_labels['price']))
-alphabet_news_labels_dict = match_news_labels(alphabet_date_news_dict, alphabet_date_labels_dict)
+alphabet_date_news_dict_train = read_news(alphabet_data_path_train)
+alphabet_labels_train = pd.read_csv(alphabet_labels_path_train)
+alphabet_date_labels_dict_train = dict(zip(alphabet_labels_train['date'], alphabet_labels_train['price']))
+alphabet_news_labels_dict_train = match_news_labels(alphabet_date_news_dict_train, alphabet_date_labels_dict_train)
 
-yahoo_date_news_dict = read_news(yahoo_data_path)
-yahoo_labels = pd.read_csv(yahoo_labels_path)
-yahoo_date_labels_dict = dict(zip(yahoo_labels['date'], yahoo_labels['price']))
-yahoo_news_labels_dict = match_news_labels(yahoo_date_news_dict, yahoo_date_labels_dict)
+yahoo_date_news_dict_train = read_news(yahoo_data_path_train)
+yahoo_labels_train = pd.read_csv(yahoo_labels_path_train)
+yahoo_date_labels_dict_train = dict(zip(yahoo_labels_train['date'], yahoo_labels_train['price']))
+yahoo_news_labels_dict_train = match_news_labels(yahoo_date_news_dict_train, yahoo_date_labels_dict_train)
+
+# Load training data
+apple_date_news_dict_test = read_news(apple_data_path_test)
+apple_labels_test = pd.read_csv(apple_labels_path_test)
+apple_date_labels_dict_test = dict(zip(apple_labels_test['date'], apple_labels_test['price']))
+apple_news_labels_dict_test = match_news_labels(apple_date_news_dict_test, apple_date_labels_dict_test)
+
+alphabet_date_news_dict_test = read_news(alphabet_data_path_test)
+alphabet_labels_test = pd.read_csv(alphabet_labels_path_test)
+alphabet_date_labels_dict_test = dict(zip(alphabet_labels_test['date'], alphabet_labels_test['price']))
+alphabet_news_labels_dict_test = match_news_labels(alphabet_date_news_dict_test, alphabet_date_labels_dict_test)
+
+yahoo_date_news_dict_test = read_news(yahoo_data_path_test)
+yahoo_labels_test = pd.read_csv(yahoo_labels_path_test)
+yahoo_date_labels_dict_test = dict(zip(yahoo_labels_test['date'], yahoo_labels_test['price']))
+yahoo_news_labels_dict_test = match_news_labels(yahoo_date_news_dict_test, yahoo_date_labels_dict_test)
+
+
 
 #==============================================================================
 # Combine dictionaries of companies
 #==============================================================================
-combined_news_labels_dict = combine_dicts(
-        [apple_news_labels_dict, alphabet_news_labels_dict, yahoo_news_labels_dict])
-corpus_text = list(combined_news_labels_dict.keys())
-labels = list(combined_news_labels_dict.values())
+# Train
+combined_news_labels_dict_train = combine_dicts(
+        [apple_news_labels_dict_train, alphabet_news_labels_dict_train, yahoo_news_labels_dict_train])
+corpus_text = list(combined_news_labels_dict_train.keys())
+labels = list(combined_news_labels_dict_train.values())
 #vocab_dict_inv = dict(zip(vocab_dict.values(), vocab_dict.keys()))
+
+# Test
+combined_news_labels_dict_test = combine_dicts(
+        [apple_news_labels_dict_test, alphabet_news_labels_dict_test, yahoo_news_labels_dict_test])
+test_corpus_text = list(combined_news_labels_dict_test.keys())
+test_labels = list(combined_news_labels_dict_test.values())
 
 #==============================================================================
 # Split the dataset
 #==============================================================================
 # Split dataset into training and test set
-corpus_text, test_corpus_text, labels, test_labels = train_test_split(
-     corpus_text, labels, test_size=0.33, random_state=42)
+#corpus_text, test_corpus_text, labels, test_labels = train_test_split(
+#     corpus_text, labels, test_size=0.33, random_state=42)
 #==============================================================================
 # Create stop word list and tokenizer
 #==============================================================================
@@ -142,15 +175,17 @@ model = LinearSVC(C=grid.best_params_['C'], penalty='l1', dual=False)
 scores = cross_val_score(model, corpus, labels, cv=5)
 print('scores: %s' % scores)
 
-training_acc = accuracy_score(labels, training_res)
-training_rec = recall_score(labels, training_res)
-training_pre = precision_score(labels, training_res)
-training_f1 = f1_score(labels, training_res)
-
-test_acc = accuracy_score(test_labels, test_res)
-test_rec = recall_score(test_labels, test_res)
-test_pre = precision_score(test_labels, test_res)
-test_f1 = f1_score(test_labels, test_res)
+with open('bigram_svc_chi2_res.txt', 'w') as f:        
+    training_acc = accuracy_score(labels, training_res)
+    training_rec = recall_score(labels, training_res)
+    training_pre = precision_score(labels, training_res)
+    training_f1 = f1_score(labels, training_res)
+    f.write('Training acc: %.3f, rec: %.3f, pre: %.3f, f1: %.3f\n' % (training_acc, training_rec, training_pre, training_f1))    
+    test_acc = accuracy_score(test_labels, test_res)
+    test_rec = recall_score(test_labels, test_res)
+    test_pre = precision_score(test_labels, test_res)
+    test_f1 = f1_score(test_labels, test_res)
+    f.write('Test acc: %.3f, rec: %.3f, pre: %.3f, f1: %.3f\n' % (test_acc, test_rec, test_pre, test_f1))    
 
 
 #==============================================================================
